0a284c44da7a095201fd06d481569952d0dd79c0
leatolu <markjtibbetts@gmail.com>
2017-09-03 23:51:53 +0200

    lastest development snapshot

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import os
 from setuptools import find_packages, setup
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gallery-app',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
-    include_package_date=True,
+    include_package_data=True,
     license='',
     description='A django app to display images in a gallery',
     long_description=README,
